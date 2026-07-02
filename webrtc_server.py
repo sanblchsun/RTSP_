150b0bba4185e139fa4e0041b749ff7ceec1ac09
Your Name <you@example.com>
2026-07-02 23:24:09 +0300

    dsfsdfgds

--- webrtc_server.py
+++ webrtc_server.py
@@ -117,8 +117,7 @@ class H264StreamTrack(VideoStreamTrack):
         self._running = True
         self._decode_lock = threading.Lock()
         self._codec = av.CodecContext.create('h264', 'r')
-        self._codec.thread_count = 1
-        self._codec.flags |= 0x0008  # AV_CODEC_FLAG_LOW_DELAY
+        self._codec.thread_count = 0
         self._request_keyframe_cb = None
         self._last_pts = None
         self._last_time = 0.0
